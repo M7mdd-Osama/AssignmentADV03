ea2fc4fc3e67d530be54a4f164514af2b6aaeb93
Mohamed Osama Ali <ana20332183@gmail.com>
2024-08-13 06:19:58 +0300

    Done

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,9 +2,67 @@
 {
     internal class Program
     {
+        public delegate int StringFuncDelegate(string str);
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Delegate
+
+            //StringFuncDelegate stringFuncDelegate = new StringFuncDelegate(StringFunctions.GetCountOfUpperChars);
+            //StringFuncDelegate stringFuncDelegate;
+
+            //stringFuncDelegate = new StringFuncDelegate(StringFunctions.GetCountOfUpperChars);
+
+            //stringFuncDelegate = StringFunctions.GetCountOfUpperChars;
+
+            //int R = stringFuncDelegate.Invoke("MaHmoud");
+            //Console.WriteLine(R);
+
+            //stringFuncDelegate += StringFunctions.GetCountOfLowerChars;
+
+            //int R = stringFuncDelegate.Invoke("MaHmoud");
+            //Console.WriteLine(R);
+
+            //stringFuncDelegate -= StringFunctions.GetCountOfLowerChars;
+
+            //int R = stringFuncDelegate/*.Invoke*/("MaHmoud");
+            //Console.WriteLine(R);
+
+            //StringFunctions.GetCountOfUpperChars("Ali");
+            #endregion
+
+            //string[] Names = { "Ahmed", "Mohamed", "Ali", "Omar" };
+            //SortingAlgorithms<string>.BubbleSort(Names, CompareFunctions.SortDescending);
+
+            //foreach (string Name in Names)
+            //{
+            //    Console.WriteLine(Name);
+            //}
+
+            //Predicate<int> predicate;
+            //predicate = TestingFunctions.Test01;
+            //Console.WriteLine(predicate(40));
+
+            ////Func<int, string> func;
+            //var func = (int X) => X.ToString();
+            //Console.WriteLine(func(42));
+
+            ////Action actionn;
+            //var actionn =   () => Console.WriteLine("Hello");
+            //actionn();
+
+            ////Action<string> action;
+            //var action = (string Name) =>  Console.WriteLine($"Hello {Name}");
+            //action("Mohamed");
+
+
         }
+    }
+    class TestingFunctions
+    {
+        public static bool Test01(int X)
+        {
+            return X > 0;
+        }
+
     }
 }
---
+++ Demo/SortingAlgorithms.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    //public delegate TResult CompareFuncDelegate<in T, out TResult>(T A, T B);
+    internal class SortingAlgorithms<T>
+    {
+        public static void BubbleSort(T[] array, Func<T, T, bool> compareFunc)
+        {
+            if (array is not null)
+            {
+                for (int i = 0; i < array.Length; i++)
+                    for (int j = 0; j < array.Length - i - 1; j++)
+                        if (compareFunc(array[j], array[j + 1]))
+                            SWAP(ref array[j], ref array[j + 1]);
+            }
+        }
+        public static void SWAP(ref T arr1, ref T arr2)
+        {
+            T Temp = arr1;
+            arr1 = arr2;
+            arr2 = Temp;
+        }
+    }
+}
